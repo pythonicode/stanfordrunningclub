f031209552b351f91eeda6ab4f2dfdf41c06c792
Anthony Riley <anthony.j.riley@gmail.com>
2026-02-03 16:50:41 +0100

    unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  images: {
+    unoptimized: true, // Disable Next.js image optimization - use Payload's image API directly
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
